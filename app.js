a762e32abb6c311b423b4aceb8824de449bed7b2
Alberto Menendez <90271858+albertomenen@users.noreply.github.com>
2023-01-28 13:31:06 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+require('./db');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+// Routers require
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+
+const app = express();
+
+// cookies and loggers
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// For deployment
+app.set('trust proxy', 1);
+app.use(
+  session({
+    name: 'project2-cookie',
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 2592000000 // 30 days in milliseconds
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL
+    })
+  }) 
+)
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// routes intro
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  if (err.status === 404) {
+    res.render('404', { path: req.url });
+  } else {
+    res.status(err.status || 500);
+    res.render('error');
+  }
+});
+
+module.exports = app;
