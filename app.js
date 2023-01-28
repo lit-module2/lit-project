eb80bb2c0d5ae281d5d7d7f0b3085d1d6e6e064d
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-01-28 13:49:40 +0100

    changed cookie name

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', 1);
 app.use(
   session({
-    name: 'project2-cookie',
+    name: 'lit-project',
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
