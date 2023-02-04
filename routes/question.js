5fc675b029cb4d4458d121c0dc49a33c37ffccae
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-02-04 11:27:00 +0100

    done, pending minor testing

--- routes/question.js
+++ routes/question.js
@@ -6,14 +6,16 @@ const UserAnswer = require('../models/UserAnswer');
 
 
 // @desc    Shows random questions
-// @route   GET /
+// @route   GET /question
 // @access  User with user role only
 router.get('/', async (req, res, next) => {
   try {
     const questions = await Question.find({});
     const randomQuestion = questions[Math.floor(Math.random() * questions.length )];
+
     const users = await User.find({});
     const shuffle = users.sort(() => 0.5 - Math.random()) //shuffles list in random order
+
     const randomUsers = shuffle.slice(0,4);
     res.render('question', { randomQuestion, randomUsers });
   } catch (error) {
@@ -21,19 +23,19 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-// @desc    App home page, user answers question
-// @route   Post /questionId
+// @desc    Registers user answers in the DB
+// @route   POST /question/:questionId
 // @access  User with user role only
-router.post('/:questionId', async function (req, res, next) {
+router.post('/:questionId', async (req, res, next) => {
   try {
     const userAsked = req.session.currentUser;
-    const { questionId } = req.params;
-    const { userAnswered,  usersIgnored} = req.body;
-    await UserAnswer.create({ questionId, userAsked, userAnswered, usersIgnored });
+    const questionId = req.params.questionId;
+    const { possibleAnswers, answer } = req.body;
+    console.log(questionId, userAsked, answer, possibleAnswers);
+    await UserAnswer.create({ questionId, userAsked, answer, possibleAnswers });
   } catch (error) {
     next(error)
   }
 });
-
 
 module.exports = router;
