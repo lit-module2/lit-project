3dfc9174712464ec533fad51b821be8362ed95f4
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-02-17 18:59:17 +0100

    all done

--- routes/submit-question.js
+++ routes/submit-question.js
@@ -16,23 +16,31 @@ router.get('/', routeProtect.isUserLoggedIn, async (req, res, next) => {
 // @access  User with user role only
 router.post('/', routeProtect.isUserLoggedIn, async (req, res, next) => {
     const { emoji, question, category, effectCheck, safeCheck } = req.body;
-    let effect = true;
-    let safe = true;
+    let effect = false;
+    let safe = false;
+    let errors = [];
     if (effectCheck === 'on') {
-        effect = false;
+        effect = true;
     }
     if (safeCheck === 'on') {
-        effect = false;
+        safe = true;
+    }
+    const regexEmoji = /\p{Extended_Pictographic}/ug;
+    if (!regexEmoji.test(emoji)) {
+        errors.push('Only emojis are accepted in the emoji field');
     }
     const currentUser = req.session.currentUser._id;
     try {
-        const uniqueQuestion = await Question.findOne({question: question});
-        if (!uniqueQuestion) {
+        const repeatedQuestion = await Question.findOne({question: question});
+        if (repeatedQuestion) {
+            errors.push('That question already exists!');
+        }
+        if (errors.length === 0) {
             const newQuestion = await Question.create({ emoji: emoji, question: question, category: category, effect: effect, safe: safe, _author: currentUser});
-            res.redirect('submit-question');
+            res.render('submit-question', { success: true });
         }
         else {
-            res.render('submit-question', { error: `That question already exists!` });
+            res.render('submit-question', { error: errors });
             return;
         }
       } catch (error) {
